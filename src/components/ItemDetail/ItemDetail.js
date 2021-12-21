1ae0667e097c4113250cd01e6c90585c904775df
NattRem <remnatt4@gmail.com>
2021-12-21 19:18:04 -0300

    Importe ItemCount en ItemDetail y configure el evento de compra

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,9 +1,31 @@
+
 import {Card, Container, Row, Col} from "react-bootstrap"
 import "../estilos/ItemDetail.css"
 import ItemCount from "../ItemListContainer/ItemCount"
+import {useState} from "react";
+import { Link } from "react-router-dom";
+import { toast, ToastContainer} from 'react-toastify';
+
 
 const ItemDetail = ({ item }) => {
+    const [goCart, setGoCart] = useState (false)
 
+    const onAdd = (cantidad)=> {
+        toast(`Agregaste ${cantidad} productos al carrito`, {
+                    position: "bottom-center",
+                    autoClose: 3000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "dark"
+                    });
+        console.log(cantidad);
+        setGoCart(true);
+    }
+
+    
     return (
         <Container className='m-5 pb-5 mx-auto bg-card card'>
             <Row >
@@ -14,7 +36,12 @@ const ItemDetail = ({ item }) => {
                         <h3 className="card-precio detail-precio">{item.precio}</h3>
                         <h4 className="detail-texto">{item.descripcion}</h4>
                     </Card.Text>
-                    <ItemCount />
+                    {!goCart ? (
+                        <ItemCount stock={item.stock} onAdd={onAdd}/>
+                        
+                    ) : (
+                        <Link to="/cart" type="button" class="btn btn-dark m-4">Ir al Carrito</Link>)}
+                    <ToastContainer />
                 </Card.Body>
                 
                 </Col>
--- src/components/ItemListContainer/ItemCount.jsx
+++ src/components/ItemListContainer/ItemCount.jsx
@@ -1,50 +1,30 @@
 import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
-import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import "../estilos/ItemDetail.css";
 
 
 
 
-function ItemCount() {
-    const inicio = 1
-    const stock = 6
 
-    const[value, setValue]= useState(inicio)
+function ItemCount({stock,onAdd}) {
 
-    const handSuma = ()=>{
-        if (value < stock){
-            setValue(value + 1)
-        }
+    const [number, setNumber] = useState(1);
+
+    const add = () => {
+        number !== stock && setNumber(number + 1);
     }
-
-    const handResta = ()=> {
-        if (value > inicio){
-            setValue(value - 1)
-        }
+    const substract = () => {
+        number !== 1 && setNumber(number - 1);
     }
-
-    const handMostrar = ()=> {
-        toast(`Agregaste ${value} productos al carrito`, {
-            position: "bottom-center",
-            autoClose: 3000,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined,
-            theme: "dark"
-            });
-    }
+    
     return (
-        <div className="">
-            <h1 className="countValue">{value}</h1>
-            <Button className="m-1 count" variant="dark" onClick={handResta}>-</Button>
-            <Button className="m-1 count" variant="dark" onClick={handSuma}>+</Button>
+        <div>
+            <h1 className="countValue">{number}</h1>
+            <Button className="m-1 count" variant="dark" onClick={substract}>-</Button>
+            <Button className="m-1 count" variant="dark" onClick={add} disabled={number === stock}>+</Button>
             <br />
-            <Button className="m-1 count" variant="dark" onClick={handMostrar}>Agregar al carrito</Button>
-            <ToastContainer />
+            <Button className="m-1 count" variant="dark" disabled={number === 0} onClick={() => onAdd(number)}>Agregar al carrito</Button>
         </div>
     )
 }
